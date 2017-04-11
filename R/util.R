d99bf48ec327e9dc5e89e93f73e0b98712a9ed1a
Zachary M. Jones <zmj@zmjones.com>
2017-04-11 15:22:12 -0400

    remove namedList

--- R/util.R
+++ R/util.R
@@ -1,9 +1,3 @@
-namedList = function(elements, names) {
-  l = list(elements)
-  names(l) = names
-  l
-}
-
 getSubsets = function(elements, proper = TRUE) {
   ret = lapply(0:(length(elements) - ifelse(proper, 1, 0)),
     function(m) combn(elements, m))
